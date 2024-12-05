97e1398d087d5cab3f4ecfe07e4419449daac4b9
elxrojo <elxrojo115@gmail.com>
2024-12-04 21:51:02 -0300

    adding cors

--- server/server.js
+++ server/server.js
@@ -13,21 +13,12 @@ const app = express();
 // Allow CORS from your frontend domain
 const allowedOrigins = ['https://tacoportfolio.netlify.app'];
 
-app.use(cors({
-	origin: function (origin, callback) {
-		// allow requests with no origin 
-		// (like mobile apps or curl requests)
-		if (!origin) return callback(null, true);
-
-		if (allowedOrigins.indexOf(origin) === -1) {
-			var msg = 'The CORS policy for this site does not ' +
-				'allow access from the specified Origin.';
-			return callback(new Error(msg), false);
-		}
-		return callback(null, true);
-	},
-	credentials: true,
-}));
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	next();
+})
 
 app.use(express.json());
 app.use('/categories', category);
