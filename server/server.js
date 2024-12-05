d12a1341bf3365e496c7edb231d35a3c84f1f9ed
elxrojo <elxrojo115@gmail.com>
2024-12-04 21:37:15 -0300

    adding cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,25 @@ dotenv.config();
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+// Allow CORS from your frontend domain
+const allowedOrigins = ['https://tacoportfolio.netlify.app'];
+
+app.use(cors({
+	origin: function (origin, callback) {
+		// allow requests with no origin 
+		// (like mobile apps or curl requests)
+		if (!origin) return callback(null, true);
+
+		if (allowedOrigins.indexOf(origin) === -1) {
+			var msg = 'The CORS policy for this site does not ' +
+				'allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	},
+	credentials: true,
+}));
+
 app.use(express.json());
 app.use('/categories', category);
 app.use('/images', images);
